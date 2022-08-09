ed3483795a857b33a5afd9334b2eefc2ca477031
İsmet Bilgiç <109041666+blgcismt@users.noreply.github.com>
2022-08-09 11:34:07 +0300

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -19,7 +19,7 @@ You can find the actions you can take below...
 5-Square root
 6-X to the power of Y
 7-log on base 10
-8-log of X on base Y
+8-log of Y on base X
 9-Factorial
 
 
@@ -42,15 +42,88 @@ while True:
         b=float(input("Please enter your second number:"))
         print("Calculating...")
         sleep(1)
-        print("{} + {} = {}:".format(a,b,a+b))
+        print("{} + {} = {}".format(a,b,a+b))
+        
     elif(action==2):
         print("Subtraction has been chosen...")
         a=float(input("Please enter the minuend:"))
         b=float(input("Please enter the subtrahend:"))
         print("Calculating...")
         sleep(1)
-        print("{} - {} = {}".format(a,b,a-b))
-
+        print("{} - {} = {}".format(a,b,a-b)
+              
+    elif(action==3):
+        print("Multiplication has been chosen...")
+        a=float(input("Please enter the multiplicand:"))
+        b=float(input("Please enter the multiplier"))
+        print("Calculating...")
+        sleep(1)
+        print("{} x {} = {}".format(a,b,a*b))
+     
+     elif(action== 4):
+        print("Division has been chosen...")
+        a=float(input("Please enter the dividend:"))
+        b=float(input("Please enter the divisor:"))
+        print("Calculating...")
+        sleep(1)
+        print("{} : {} = {}".format(a,b,a/b))
+     
+     elif(action==5):
+        print("Square root has been chosen...")
+        a=float(input("Please enter the number you want to square root:"))
+        if(a<0):
+            print("Invalid request!")
+        else:
+            print("Calculating...")
+            sleep(1)
+            print("The square root of {} is {}".format(a,a**0.5))
+              
+      elif(action==6):
+        print("Exponentiation has been chosen ...")
+        print("Please do not enter a negative power")
+        a=float(input("Please enter the base:"))
+        b=float(input("Please enter the power:"))
+        if(b<0):
+            print("Invalid request!")
+        else:
+            print("Calculating...")
+            sleep(1)
+            print("{} to the power of {} is {}".format(a,b,pow(a,b)))
+              
+      elif(action==7):
+        print("Logarithm operation in base ten has been chosen...")
+        a=float(input("Please enter the number you want to logarithm in base ten:"))
+        if(a<=0):
+            print("Invalid request!")
+        else:
+            print("Calculating...")
+            sleep(1)
+            print("Logarithm {} in base 10 equals {}:".format(a,log10(a)))
+              
+      elif(action==8):
+        print("Logarithm of Y in base X was chosen...")
+        a=float(input("X:"))
+        if(a==0 or a==1):
+            print("Invalid request!")
+        b=float(input("Y:"))
+        if(b<=0):
+            print("Invalid request!")
+        else:
+            print("Calculating...")
+            sleep(1)
+            print("Logarithm {} in base {} equals {}:".format(b,a,log(b,a)))
+       
+      elif(action==9):
+        print("Factorial finding has been chosen...")
+        a=int(input("Please enter the number you want to get the factorial of:"))
+        if(a<0):
+            print("Invalid request!")
+        else:
+            print("Calculating...")
+            sleep(1)
+            print("Factorial {} = {}".format(a,factorial(a)))
+    else:
+        print("Invalid request! Please select a valid action.")
   
   
           
