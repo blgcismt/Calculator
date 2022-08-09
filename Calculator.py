bde76dbc4d6769abdb04dc926ca1c4ce1bc9cbce
İsmet Bilgiç <109041666+blgcismt@users.noreply.github.com>
2022-08-09 10:52:58 +0300

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,27 @@
 from math import*
+from time import sleep
+
+
+print("""
+
+
+Welcome to the calculator....
+
+You can find the actions you can take below;
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+5. Square root
+6. X to the power Y
+7. log on base 10
+8. log of X on base Y
+9. Factorial
+
+0. Exit the program
+
+""")
+
+
+while True:
+  
