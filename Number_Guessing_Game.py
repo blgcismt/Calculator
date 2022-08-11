e1436857d7de3c9506ad2925b253579a97e06e09
İsmet Bilgiç <109041666+blgcismt@users.noreply.github.com>
2022-08-11 23:40:21 +0300

    Update and rename Number_Guessing_Game to Number_Guessing_Game.py

---
+++ Number_Guessing_Game.py
@@ -0,0 +1,49 @@
+import time
+import random
+
+print(""" 
+
+Welcome to the Number Guessing Game!
+
+Guess the number between 1 and 1000....
+
+You have 10 guesses....
+
+""")
+
+random_number = random.randint(1,1000)
+guesses = 10
+
+while True :
+  
+  guess = int(input("Please enter your guess:"))
+  
+  if guess < random_number :
+    print("Checking your number...")
+    time.sleep(2)
+    print("Please enter a bigger number...")
+    guesses -=1
+    print("Number of guesses left : ",guesses)
+    
+  elif guess > random_number :
+    print("Checking your number...")
+    time.sleep(2)
+    print("Please enter a smaller number...")
+    guesses -=1
+    print("Number of guesses left : ",guesses)
+    
+  else:
+    print("Checking your number...")
+    time.sleep(2)
+    print("Congratulations, your answer is correct! Your number is:",random_number)
+    break
+    
+  if guesses == 0 :
+    print("You're out of guesses, you've lost the game!")
+    break
+  
+     
+    
+ 
+    
+     
