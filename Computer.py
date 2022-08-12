bc6cafdeaf903a37c00004ab9ff7c75b45426929
İsmet Bilgiç <109041666+blgcismt@users.noreply.github.com>
2022-08-12 13:56:54 +0300

    Update Computer.py

--- Computer.py
+++ Computer.py
@@ -171,3 +171,51 @@ class Computer:
                         print("Factorial {} = {}".format(a, factorial(a)))
                 else:
                     print("Invalid request! Please select a valid action.")
+        elif self.status == "Off":
+            print("Computer is turned off, please open it to access the calculator...")
+
+    def guessing_game(self):
+        
+        if self.status == "On":
+            print(""" 
+            Welcome to the Number Guessing Game!
+            Guess the number between 1 and 1000....
+            You have 10 guesses....
+            """)
+
+            random_number = random.randint(1, 1000)
+            guesses = 10
+
+            while True:
+
+                guess = int(input("Please enter your guess:"))
+
+                if guess < random_number:
+                    print("Checking your number...")
+                    time.sleep(1)
+                    print("Please enter a bigger number...")
+                    guesses -= 1
+                    print("Number of guesses left : ", guesses)
+
+                elif guess > random_number:
+                    print("Checking your number...")
+                    time.sleep(1)
+                    print("Please enter a smaller number...")
+                    guesses -= 1
+                    print("Number of guesses left : ", guesses)
+
+                else:
+                    print("Checking your number...")
+                    time.sleep(1)
+                    print("Congratulations, your answer is correct! Your number is:", random_number)
+                    break
+
+                if guesses == 0:
+                    print("You're out of guesses, you've lost the game!")
+                    break
+                    
+        elif self.status == "Off":
+            print("Computer is turned off, please open it to access the number guessing game...")
+      
+          
+            
